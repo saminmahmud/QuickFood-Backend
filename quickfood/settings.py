0374dbb39f337dd18bcd9cc7ed38c3d00a74b7d5
saminmahmud <samin.mahmud18@gmail.com>
2025-03-29 23:25:08 +0600

    added CSRF url

--- quickfood/settings.py
+++ quickfood/settings.py
@@ -56,13 +56,13 @@ if DEBUG:
 
 AUTH_USER_MODEL = 'users.User'
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:3000", 
-    "https://*.railway.app"
-]
-CSRF_TRUSTED_ORIGINS=["http://localhost:5173","http://localhost:3000", "https://*.railway.app"]
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://localhost:3000", 
+#     "https://*.railway.app"
+# ]
+CSRF_TRUSTED_ORIGINS=["http://localhost:5173","http://localhost:3000", "https://quickfood-backend-naoe.onrender.com"]
 CORS_ALLOW_CREDENTIALS = True
 
 
