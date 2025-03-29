8b295a37aed0fc4f1200b8ce06a055518ec83774
saminmahmud <samin.mahmud18@gmail.com>
2025-03-29 23:02:35 +0600

    fix media url

--- quickfood/urls.py
+++ quickfood/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from users.views import CustomTokenObtainPairView, CustomTokenRefreshView
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('order/', include('order.urls')),
@@ -12,4 +16,4 @@ urlpatterns = [
     path('auth/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
